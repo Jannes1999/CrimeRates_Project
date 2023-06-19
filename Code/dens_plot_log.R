fdfa552bd6c7b94ea0bc283b71b0366ecfc00c0d
Jannes1999 <janneseloff1999@gmail.com>
2023-06-19 19:29:51 +0200

    log

---
+++ Code/dens_plot_log.R
@@ -0,0 +1,13 @@
+dens_plot_log <- function(alpha, binwidth){
+  
+Data1$`Crime Rate` = log(Data1$`Crime Rate`)
+x <- Data1 %>% ggplot()+
+  geom_histogram(aes(y= after_stat(density),x =`Crime Rate` ), colour = "black", fill="#CDCDCD", binwidth = 0.2) +
+  geom_density(aes(x =`Crime Rate`)) +
+  geom_vline(aes(xintercept=mean(`Crime Rate`)),
+             color="blue", linetype="dashed", linewidth = 1) +
+  labs(title = "log Crime rate density")
+
+print(x)
+
+}
